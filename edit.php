462716da48a710534933de6d00f1ca726955e5ea
hemendra birla <78023128+hemendrabirla@users.noreply.github.com>
2022-11-21 13:05:32 +0530

    new

--- edit.php
+++ edit.php
@@ -1,31 +1,3 @@
-
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "shop";
-$id=$_POST['edit'];
-$query = "SELECT * FROM product WHERE id = $id"; 
-$result = mysqli_query($conn, $query) or die ( mysqli_error());
-$row = mysqli_fetch_assoc($result);
-
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error){
-  die("Connection failed: " . $conn->connect_error);
-} 
-$sql = "UPDATE `product` SET $products  = 'product_name', $quantity = 'product_quantity' WHERE id=$id";
-
-if ($conn->query($sql) === TRUE) {
-  echo "Record updated successfully";
-} else {
-  echo "Error updating record: " . $conn->error;
-}
-
-$conn->close();?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -33,10 +5,40 @@ $conn->close();?>
 <body>
 <form action="" method="post">
 <label>Product Name:</label>
-<input class="input" name="products" type="text" value="<?php echo $row['products'];?>">
+<input class="input" name="products" type="text" value="<?php echo $row['product_name'];?>">
 <label>Product Quantity:</label>
-<input class="input" name="quantity" type="text" value="<?php echo $row['quantity'];?>" required>
-<input class="submit" name="submit" type="submit" value="Insert" required>
+<input class="input" name="quantity" type="text" value="<?php echo $row['product_quantity'];?>">
+<input class="submit" name="update" type="submit" value="update" required>
 </form>
 </body>
 </html>
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shop";
+$row['product_name'] = "";
+$row['product_quantity'] = "";
+
+$id = $_REQUEST['edit'];
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error){
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+if (isset($_POST['update'])) {
+	  echo $products = $_POST['products'];
+	  echo $quantity = $_POST['quantity'];
+
+	 
+ $sql =  "UPDATE `product` SET product_name = `$products`, product_quantity = `$quantity` WHERE id = $id";
+      $result = mysqli_query($conn ,$sql);
+
+	
+}
+?>
